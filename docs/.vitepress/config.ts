0193070d1edeb5b2de2b87de27245db0020c79de
chenkaijia <kaijia.chen@delonixmail.com>
2025-08-15 14:29:26 +0800

    Deploy VitePress site to Pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'My VitePress Blog',
   description: 'A VitePress Site',
+  base: '/vitepress-blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
